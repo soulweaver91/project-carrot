b0427f4f9b25343c5b840322affa0d961efd0eda
Soulweaver <soulweaver@hotmail.fi>
2016-06-20 22:27:56 +0300

    Replace the remaining uses of function pointers
    Ref #41

--- src/gamestate/AnimationUser.h
+++ src/gamestate/AnimationUser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <functional>
 #include <QString>
 #include <SFML/Graphics.hpp>
 #include "TimerUser.h"
@@ -12,14 +13,14 @@ class AnimationInstance;
 class AnimationUser;
 class GameView;
 
-typedef void(AnimationUser::*AnimationCallbackFunc)(std::shared_ptr<AnimationInstance>);
+typedef std::function<void()> AnimationCallbackFunc;
 
 class AnimationInstance : public TimerUser, public std::enable_shared_from_this<AnimationInstance> {
 public:
     AnimationInstance(AnimationUser* const owner);
     void drawCurrentFrame(sf::RenderTarget& target);
     void setAnimation(std::shared_ptr<GraphicResource> newAnimation, const AnimStateT& newState = AnimState::IDLE,
-        AnimationCallbackFunc cb = nullptr);
+        AnimationCallbackFunc cb = []() {});
     void setSpritePosition(const sf::Vector2f& position, const sf::Vector2f& scale = { 1.0, 1.0 });
     AnimStateT getAnimationState();
     std::shared_ptr<const GraphicResource> getAnimation();
@@ -51,7 +52,7 @@ public:
     void loadAnimationSet(QMap<QString, std::shared_ptr<GraphicResource>>& animations);
     void advanceAnimationTimers();
     virtual bool setAnimation(AnimStateT state);
-    virtual bool setTransition(AnimStateT state, bool cancellable, AnimationCallbackFunc callback = nullptr);
+    virtual bool setTransition(AnimStateT state, bool cancellable, AnimationCallbackFunc callback = []() {});
     virtual void animationFinishedHook(std::shared_ptr<AnimationInstance> animation);
 
 protected:
